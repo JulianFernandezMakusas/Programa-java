6ad38634b54ef1591cb928cb5c1065e0e0afdb72
juliku <38531770+JulianFernandezMakusas@users.noreply.github.com>
2018-08-27 22:07:18 -0300

    Add files via upload

--- Colecciones/src/com/utn/vista/Main.java
+++ Colecciones/src/com/utn/vista/Main.java
@@ -6,23 +6,32 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Set;
+import java.util.TreeMap;
 import java.util.TreeSet;
 
 import javax.swing.JOptionPane;
 
+import com.sun.javafx.collections.MappingChange.Map;
+
+import jdk.nashorn.internal.scripts.JO;
+
 public class Main {
 
 	public static void main(String[] args) {
+		boolean datosAlumnoBoolean = true;
+		boolean datosProfesorBoolean = true;
 		Persona profesor = null;
 		Persona alumno = null;
-		int Dni;
-		String DniS;
-		Set<Persona> listaPersonas = new TreeSet<>();
+		int dni = 0;
+		String dniS;
+		TreeMap<Integer, Persona> listaProfesores = new TreeMap<Integer, Persona>();
+		TreeMap<Integer, Persona> listaAlumnos = new TreeMap<Integer, Persona>();
+		
 
 		Path bDniAlumn = Paths
-				.get("C:\\Users\\Java\\Desktop\\Padron de profesores Legajos de alumnos2.0\\Datos_Alumno.ser");
+				.get("C:\\Users\\Raul\\Desktop\\Padron de profesores Legajos de alumnos2.0\\Datos_Alumno.ser");
 		Path bDniProf = Paths
-				.get("C:\\Users\\Java\\Desktop\\Padron de profesores Legajos de alumnos2.0\\Datos_Profesor.ser");
+				.get("C:\\Users\\Raul\\Desktop\\Padron de profesores Legajos de alumnos2.0\\Datos_Profesor.ser");
 		if (Files.exists(bDniProf) == false) {
 			System.out.println("No se cargaron profesores");
 		} else {
@@ -33,28 +42,58 @@ public class Main {
 		} else {
 			System.out.println("Abriendo archivo de Alumnos");
 		}
-		while (true) {
+		while (datosAlumnoBoolean = true) {
 			try {
-				ObjectInputStream datosAlumno = new ObjectInputStream(Files.newInputStream(bDniAlumn));
 				try {
-					alumno = (Alumno)datosAlumno.readObject();
-					System.out.println("Jia");
-				} catch (ClassNotFoundException e) {
-
-				}
-				ObjectInputStream datosProfesor = new ObjectInputStream(Files.newInputStream(bDniProf));
-				try {
-					profesor =(Profesor) datosProfesor.readObject();
-					System.out.println("Jia");
+					ObjectInputStream datosAlumno = new ObjectInputStream(Files.newInputStream(bDniAlumn));
+					alumno = (Alumno) datosAlumno.readObject();
+					listaAlumnos.put(alumno.getDni(), alumno);
 				} catch (ClassNotFoundException e) {
 
 				}
 			} catch (IOException e) {
-				System.out.println("Final del archivo");
+				e.printStackTrace();
+				datosAlumnoBoolean = false;
+			}
+			while (datosProfesorBoolean = true) {
+				try {
+					ObjectInputStream datosProfesor = new ObjectInputStream(Files.newInputStream(bDniProf));
+					try {
+						profesor = (Profesor) datosProfesor.readObject();
+						listaProfesores.put(profesor.getDni(), profesor);
+					} catch (ClassNotFoundException e) {
+
+					}
+				} catch (IOException e) {
+					datosProfesorBoolean = false;
+				}
+			}
+		}
+		int optionType = 0;
+		String[] options = {"Alumnos","Profesores"};
+		JOptionPane.showMessageDialog(null, "Bienvenido");
+		int seleccion = JOptionPane.showOptionDialog(null, "Ingrese la categoria en la que desea buscar: ", "Programa", optionType, 0, null, options, 0);
+		
+		switch (seleccion) {
+		case 0://alumnos
+			dniS = JOptionPane.showInputDialog("Ingrese el DNI que desea encontrar");
+			dni = Integer.parseInt(dniS);			
+			for (int i = 0; i < listaAlumnos.size(); i++) {
+				if (listaAlumnos.equals(dni)) {
+						JOptionPane.showMessageDialog(null, listaAlumnos.get(i));
+				}else {
+					System.out.println("no hay nada");
+				}
+			}
+			break;
+		case 1://profesores
+			dniS = JOptionPane.showInputDialog("Ingrese el DNI que desea encontrar");
+			dni = Integer.parseInt(dniS);
+			for (int i = 0; i < listaProfesores.size(); i++) {
+				
 			}
 			break;
 		}
-		System.out.println(alumno.getDni());
-		System.out.println(profesor.getDni());
+		
 	}
 }
